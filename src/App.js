90ab5eb63478c8bc345deb794cde888a89603c82
charan <charanvarikuti115@gmail.com>
2026-02-06 18:47:39 +0530

    Message added

--- src/App.js
+++ src/App.js
@@ -120,6 +120,7 @@ function Menu() {
         <div className="menu">
             <h2 style={{ textAlign: "center", marginTop: "10px" }}><u>Our Menu</u></h2>
             <p>Lists starting...1123</p>
+            <p>List Starting ---1123</p>
             <p>Hi, Good Morning</p>
             <p>Now merging the code from master to feature-footer</p>
             <ul className="pizzas">

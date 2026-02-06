e4c113f5f265182ecf10d22d9f494df8f2d3e14b
charan <charanvarikuti115@gmail.com>
2026-02-06 13:32:32 +0530

    Tag added

--- src/App.js
+++ src/App.js
@@ -120,6 +120,7 @@ function Menu() {
         <div className="menu">
             <h2 style={{ textAlign: "center", marginTop: "10px" }}><u>Our Menu</u></h2>
             <p>Lists starting...1123</p>
+            <p>Hi, Good Morning</p>
             <ul className="pizzas">
                 {/* Now using the Ternary Operator */}
                 {pizzaLength > 0 ?
